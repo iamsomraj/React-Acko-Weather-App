02840bfcf7e749d787729b808372879f33f149c7
Somraj Mukherjee <iamsomraj@gmail.com>
2021-12-04 17:02:31 +0530

    👌 IMPROVE: container layout with body and heading

--- src/containers/AboutContainer/AboutContainer.tsx
+++ src/containers/AboutContainer/AboutContainer.tsx
@@ -3,15 +3,21 @@ import UtilContainer from "../../components/UtilContainer/UtilContainer";
 const AboutContainer = () => {
   return (
     <UtilContainer>
-      <h1>About</h1>
-      <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus unde
-        maiores dolores architecto quas corporis provident dolor porro
-        consectetur nobis veniam animi sequi fuga fugiat quasi, consequuntur
-        nostrum, harum velit! Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Blanditiis ducimus minus enim ipsam sit vitae animi
-        quasi facere cupiditate. Soluta provident mollitia commodi similique
-        fugit amet quam quas in doloribus!
+      <div className="flex justify-center items-center">
+        <div className="w-full lg:w-1/2 space-y-5">
+          <div className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-teal-500 via-purple-600 to-purple-500">
+            About
+          </div>
+          <div>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            unde maiores dolores architecto quas corporis provident dolor porro
+            consectetur nobis veniam animi sequi fuga fugiat quasi, consequuntur
+            nostrum, harum velit! Lorem ipsum, dolor sit amet consectetur
+            adipisicing elit. Blanditiis ducimus minus enim ipsam sit vitae
+            animi quasi facere cupiditate. Soluta provident mollitia commodi
+            similique fugit amet quam quas in doloribus!
+          </div>
+        </div>
       </div>
     </UtilContainer>
   );
--- src/containers/TermsContainer/TermsContainer.tsx
+++ src/containers/TermsContainer/TermsContainer.tsx
@@ -3,28 +3,36 @@ import UtilContainer from "../../components/UtilContainer/UtilContainer";
 const TermsContainer = () => {
   return (
     <UtilContainer>
-      <h1>Terms</h1>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non vel sequi,
-        iure nihil ipsum ab tempora aliquid! Beatae, harum asperiores aut nihil
-        commodi dolorem fugiat repudiandae saepe quasi velit facere!
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum culpa
-        repudiandae aliquid minima dolor eligendi corrupti hic ex veritatis
-        nostrum. Impedit accusantium esse ad? Exercitationem ullam eligendi
-        laborum delectus sed!
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non vel sequi,
-        iure nihil ipsum ab tempora aliquid! Beatae, harum asperiores aut nihil
-        commodi dolorem fugiat repudiandae saepe quasi velit facere!
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum culpa
-        repudiandae aliquid minima dolor eligendi corrupti hic ex veritatis
-        nostrum. Impedit accusantium esse ad? Exercitationem ullam eligendi
-        laborum delectus sed!
+      <div className="flex justify-center items-center">
+        <div className="w-full lg:w-1/2 space-y-5">
+          <div className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-teal-500 via-purple-600 to-purple-500">
+            Terms
+          </div>
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non vel
+            sequi, iure nihil ipsum ab tempora aliquid! Beatae, harum asperiores
+            aut nihil commodi dolorem fugiat repudiandae saepe quasi velit
+            facere!
+          </div>
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum culpa
+            repudiandae aliquid minima dolor eligendi corrupti hic ex veritatis
+            nostrum. Impedit accusantium esse ad? Exercitationem ullam eligendi
+            laborum delectus sed!
+          </div>
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non vel
+            sequi, iure nihil ipsum ab tempora aliquid! Beatae, harum asperiores
+            aut nihil commodi dolorem fugiat repudiandae saepe quasi velit
+            facere!
+          </div>
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum culpa
+            repudiandae aliquid minima dolor eligendi corrupti hic ex veritatis
+            nostrum. Impedit accusantium esse ad? Exercitationem ullam eligendi
+            laborum delectus sed!
+          </div>
+        </div>
       </div>
     </UtilContainer>
   );
