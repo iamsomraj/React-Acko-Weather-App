884cc7d6b6e630f1fc6fde76088bad6dae91befb
Somraj Mukherjee <iamsomraj@gmail.com>
2021-12-04 19:51:53 +0530

    📦 NEW: Footer bottom unit test added

--- src/components/FooterBottom/FooterBottom.tsx
+++ src/components/FooterBottom/FooterBottom.tsx
@@ -4,10 +4,13 @@ function FooterBottom() {
    */
   return (
     <div className="py-4 mt-16 md:flex justify-between border-t text-gray-500 border-gray-300 text-sm text-center md:text-left space-y-4 md:space-y-0">
-      <div>Copyright &copy; {new Date().getFullYear()}</div>
+      <div data-testid="copyright-element">
+        Copyright &copy; {new Date().getFullYear()}
+      </div>
       <div>
         Made with 💜 by{" "}
         <a
+          data-testid="link-element"
           className="text-purple-400 font-medium"
           href="https://github.com/iamsomraj/"
         >
