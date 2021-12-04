76b4c24f8de67f3c0b381e8587b46d81062b768d
Somraj Mukherjee <iamsomraj@gmail.com>
2021-12-04 19:05:38 +0530

    👌 IMPROVE: changed page title

--- src/containers/AboutContainer/AboutContainer.tsx
+++ src/containers/AboutContainer/AboutContainer.tsx
@@ -9,7 +9,7 @@ const AboutContainer = () => {
           <div className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-teal-500 via-purple-600 to-purple-500">
             About
           </div>
-          <div className="leading-relaxed text-lg">
+          <div className="leading-relaxed text-lg text-justify">
             Lorem,<Highlighter> ipsum </Highlighter>dolor sit amet consectetur
             adipisicing elit. Ducimus unde maiores dolores architecto quas
             corporis provident dolor porro consectetur nobis veniam animi sequiz
