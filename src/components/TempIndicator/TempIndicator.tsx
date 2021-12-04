ac3ddcd028515d5e97c9d909a0191d908885f346
Somraj Mukherjee <iamsomraj@gmail.com>
2021-12-05 01:04:13 +0530

    👌 IMPROVE: temp indicator comp

--- src/components/TempIndicator/TempIndicator.tsx
+++ src/components/TempIndicator/TempIndicator.tsx
@@ -3,22 +3,22 @@ import { ITempIndicatorProps } from "../../types";
 const TempIndicator: React.FC<ITempIndicatorProps> = ({ val, index }) => {
   if (index !== 2) return null;
 
-  let background = `bg-yellow-500`;
-
   /**
    * index === 2 means Real Feel
    */
   if (index === 2) {
     if (val < 5) {
-      background = `bg-teal-500`;
+      return <span className={`block h-3 w-3 rounded-full bg-teal-500`}></span>;
     }
 
     if (val < 20) {
-      background = `bg-green-500`;
+      return (
+        <span className={`block h-3 w-3 rounded-full bg-green-500`}></span>
+      );
     }
   }
 
-  return <span className={`block h-3 w-3 rounded-full ${background}`}></span>;
+  return <span className={`block h-3 w-3 rounded-full bg-yellow-500`}></span>;
 };
 
 export default TempIndicator;
